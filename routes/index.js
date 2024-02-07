7e6918276fafe1730d1a0fad5afc7d7397e6dbf4
Alan Vasquez <alanvasloadev@gmail.com>
2024-02-07 00:36:48 -0600

    verificacion de fecha

--- routes/index.js
+++ routes/index.js
@@ -28,40 +28,5 @@ router.post('/tips', (req, res) => {
   res.json(newTip);
 });
 
-// Ruta para obtener una tarea por ID
-router.get('/tasks/:id', (req, res) => {
-  const taskId = parseInt(req.params.id);
-  const task = tasks.find((t) => t.id === taskId);
-  if (!task) {
-    res.status(404).json({ error: 'Tarea no encontrada' });
-  } else {
-    res.json(task);
-  }
-});
-
-// Ruta para actualizar una tarea por ID
-router.put('/tasks/:id', (req, res) => {
-  const taskId = parseInt(req.params.id);
-  const updatedTask = req.body;
-  const existingTaskIndex = tasks.findIndex((t) => t.id === taskId);
-  if (existingTaskIndex === -1) {
-    res.status(404).json({ error: 'Tarea no encontrada' });
-  } else {
-    tasks[existingTaskIndex] = { ...tasks[existingTaskIndex], ...updatedTask };
-    res.json(tasks[existingTaskIndex]);
-  }
-});
-
-// Ruta para eliminar una tarea por ID
-router.delete('/tasks/:id', (req, res) => {
-  const taskId = parseInt(req.params.id);
-  const existingTaskIndex = tasks.findIndex((t) => t.id === taskId);
-  if (existingTaskIndex === -1) {
-    res.status(404).json({ error: 'Tarea no encontrada' });
-  } else {
-    const deletedTask = tasks.splice(existingTaskIndex, 1);
-    res.json(deletedTask[0]);
-  }
-});
 
 module.exports = router;
